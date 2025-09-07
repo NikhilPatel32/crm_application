f8da671c99c4ad86c66f93f32708c84c31a29cba
NikhilPatel <leakee134@gmail.com>
2025-09-07 21:30:40 +0530

    add user model

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const connectToDB = require('./database/db');
+
+const app = express();
+
+//making connection to database
+connectToDB();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/' , (req , res) => {
+    res.json({
+        message: 'CRM Application is running...'
+    })
+})
+
+app.listen(PORT , () => {
+    console.log(`server is running on port ${PORT}`);
+})
+
