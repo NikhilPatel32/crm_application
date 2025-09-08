e914a318a70f18c43e66a00172c3ad6b8300fdbb
NikhilPatel <leakee134@gmail.com>
2025-09-08 18:07:25 +0530

    setup frontend structure and TailwindCSS

---
+++ frontend/crm_frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      Hello World starting frontend...
+    </div>
+  )
+}
+
+export default App
